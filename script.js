fb0330eeec3c360ac068d2dc37dd945cbb946bdc
Alan D. E. Cruse <alancruse@tutanota.com>
2020-05-25 14:08:15 +0200

    Add basic math operators

---
+++ script.js
@@ -0,0 +1,26 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+}
+
+// console.log(add(2, 2))
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+}
+
+const multiply = function(num1, num2) {
+	// let multi = 1
+	// arr.forEach(element => {
+	// 	multi *= element; 
+	// });
+    // return multi;
+    return num1 * num2;
+}
+
+const divide = function(num1, num2) {
+    return num1 / num2; 
+}
+
+const operate = function(operator, num1, num2) {
+    return operator(num1, num2);
+}
