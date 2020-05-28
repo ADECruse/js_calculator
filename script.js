36f8a422ee76f4139c99925869ee725a6877ecab
Alan D. E. Cruse <alancruse@tutanota.com>
2020-05-28 16:45:34 +0200

    Add logic to allow string of operators

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let displayValue = "";
+let display2 = [];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 const display = document.querySelector('#display-result');
 const digit = document.querySelectorAll('.digit');
-const addButton = document.querySelector('.operator');
+const operator = document.querySelector('.operator');
 const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
 
@@ -24,49 +26,91 @@ const divide = function(num1, num2) {
 };
 
 let storedValues = {
-    operator: null,
+    operator: add,
     firstNum: 0,
     secondNum: 0,
 };
+ //reduce ?
+// const nextFunction = function() {
 
-const nextFunction = function() {
-    storedValues.firstNum = parseFloat(displayValue);
-    displayValue = ""
-    storedValues.operator = add;
-};
+    
+//     storedValues.firstNum = parseFloat(displayValue);
+//     displayValue = ""
+//     storedValues.operator = add;
+// };
+
+
+// const operate = function(arr) {
+//     let a = arr.firstNum;
+//     let b = arr.secondNum;
+//     let c = arr.operator(a,b);
+//     // return c(a, b);
+//     console.log(c);
+//     display.textContent = c;
+// };
 
 const operate = function(arr) {
     let a = arr.firstNum;
     let b = arr.secondNum;
     let c = arr.operator(a,b);
-    // return c(a, b);
     console.log(c);
-    display.textContent = c;
+    return c
 };
 
-
+const nextFunction = function() {
+    storedValues.secondNum = parseFloat(display.textContent);
+    console.log({storedValues});
+    storedValues.firstNum = operate(storedValues);
+    storedValues.operator = add;
+    display.textContent = storedValues.firstNum;
+    display2 = [];
+};
 
 //Button functions
 
+// digit.forEach(event => event.addEventListener('click', e => {
+//     displayValue = displayValue + e.target.textContent;
+//     display.textContent = displayValue;
+// }));
+
 digit.forEach(event => event.addEventListener('click', e => {
-    displayValue = displayValue + e.target.textContent;
-    display.textContent = displayValue;
+    // displayValue = displayValue + e.target.textContent
+    display2.push(e.target.textContent)
+    display.textContent = display2.reduce(reducer);
 }));
 
-addButton.addEventListener('click', nextFunction);
+operator.addEventListener('click', nextFunction);
+
+// equals.addEventListener('click', function (params) {
+//     storedValues.secondNum = parseFloat(displayValue);
+//     operate(storedValues)
+// }); 
 
 equals.addEventListener('click', function (params) {
-    storedValues.secondNum = parseFloat(displayValue);
-    operate(storedValues)
+    storedValues.secondNum = parseFloat(display.textContent);
+    display.textContent = operate(storedValues)
+    storedValues.firstNum = 0;
+    storedValues.secondNum = 0;
+    console.table(storedValues);
 }); 
+
+// clear.addEventListener('click', function() {
+//     storedValues = {
+//         operator: null,
+//         firstNum: 0,
+//         secondNum: 0,
+//     }
+//     displayValue = ""
+//     display.textContent = "0";
+// } )
 
 clear.addEventListener('click', function() {
     storedValues = {
-        operator: null,
+        operator: add,
         firstNum: 0,
         secondNum: 0,
     }
-    displayValue = ""
+    display2 = [];
     display.textContent = "0";
 } )
-    
+
